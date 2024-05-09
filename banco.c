e905dedc03e8b6b7b4ece649afa4659ff2b3b570
raniellyaff <161093152+raniellyaff@users.noreply.github.com>
2024-05-09 23:31:36 +0000

    função de listar clientes

--- banco.c
+++ banco.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 ERROS criar(Cliente clientes[], int *pos) {
-  if (*pos >= TOTAL) { // Verifica se já atingiu o limite de clientes
+  if (*pos >= TOTAL) {      // Verifica se já atingiu o limite de clientes
     return LIMITE_CLIENTES; // Retorna o erro caso tenha atingido o limite
   }
 
@@ -12,19 +12,16 @@ ERROS criar(Cliente clientes[], int *pos) {
 
   do {
     printf("Entre com o seu CPF (11 dígitos): ");
-    fgets(clientes[*pos].cpf, sizeof(clientes[*pos].cpf), stdin); // Lê o CPF do cliente
-    clientes[*pos].cpf[strcspn(clientes[*pos].cpf, "\n")] = '\0'; // Remove o caractere de nova linha
+    fgets(clientes[*pos].cpf, sizeof(clientes[*pos].cpf),stdin); // Lê o CPF do cliente
+    clientes[*pos].cpf[strcspn(clientes[*pos].cpf, "\n")] ='\0'; // Remove o caractere de nova linha
 
     if (strlen(clientes[*pos].cpf) != 11) { // Verifica se o CPF tem 11 dígitos
       printf("O CPF deve conter 11 dígitos.\n");
-      clearBuffer(); // Limpa o buffer para remover caracteres extras
     }
   } while (strlen(clientes[*pos].cpf) != 11); // Continua pedindo o CPF até que tenha 11 dígitos
 
   printf("A sua conta será COMUM ou PLUS? ");
   fgets(clientes[*pos].conta, CONTA, stdin); // Lê o tipo de conta do cliente
-
-  clearBuffer(); // Limpa o buffer para evitar problemas na leitura futura
 
   printf("Qual será o valor inicial da sua conta? ");
   scanf("%d", &clientes[*pos].saldo); // Lê o saldo inicial do cliente
@@ -32,24 +29,21 @@ ERROS criar(Cliente clientes[], int *pos) {
   clearBuffer(); // Limpa o buffer novamente
 
   do {
-    printf("Crie uma senha de até 8 dígitos: ");
-    fgets(clientes[*pos].senha, sizeof(clientes[*pos].senha), stdin); // Lê a senha do cliente
+    printf("Crie uma senha com 8 dígitos: ");
+    fgets(clientes[*pos].senha, sizeof(clientes[*pos].senha),stdin); // Lê a senha do cliente
 
     // Remove a quebra de linha se existir
     clientes[*pos].senha[strcspn(clientes[*pos].senha, "\n")] = '\0';
 
     if (strlen(clientes[*pos].senha) != 8) { // Verifica se a senha tem 8 dígitos
       printf("A senha deve ter 8 dígitos.\n");
-      clearBuffer(); // Limpa o buffer para remover caracteres extras
     }
   } while (strlen(clientes[*pos].senha) != 8); // Continua pedindo a senha até que tenha 8 dígitos
 
   *pos = *pos + 1; // Incrementa a posição do array de clientes
-  printf("\nConta Criada com sucesso!\nSeja bem vindo ao CBank <3\n"); // Mensagem de sucesso
-
+  printf("\nConta criada com sucesso!\nSeja bem vindo ao CBank <3\n"); // Mensagem de sucesso
   return OK; // Retorna OK para indicar que a operação foi concluída com sucesso
 }
-
 
 ERROS deletar(Cliente clientes[], int *pos) {
   char cpf[CPF]; // Variável para armazenar o CPF a ser excluído
@@ -72,16 +66,39 @@ ERROS deletar(Cliente clientes[], int *pos) {
       // Decrementa o número total de clientes
       *pos = *pos - 1;
       printf("Cliente excluído com sucesso.\n");
+
+      // Se não houver mais clientes, redefine pos como 0
+      if (*pos == 0) {
+        *pos = 0;
+      }
+
       return OK; // Retorna OK para indicar que o cliente foi excluído com sucesso
     }
   }
-
   // Se não encontrar o cliente com o CPF fornecido, exibe mensagem de erro
   printf("Cliente com CPF %s não encontrado.\n", cpf);
   return CLIENTE_NAO_ENCONTRADO; // Retorna erro indicando que o cliente não foi encontrado
 }
 
+ERROS listar(Cliente clientes[], int *pos) {
+  if (*pos == 0) {
+    return SEM_CLIENTES;
+  }
+
+  for (int i = 0; i < *pos; i++) { // inicial o loop
+    printf("\nNome: %s", clientes[i].nome);
+    printf("CPF: %s\n", clientes[i].cpf);
+    printf("Conta: %s", clientes[i].conta);
+    printf("Saldo: %d\n", clientes[i].saldo);
+    printf("Senha: %s\n", clientes[i].senha);
+  }
+
+  printf("\nClientes listados com sucesso! <3\n");
+  return OK;
+}
+
 void clearBuffer() {
   int c;
-  while ((c = getchar()) != '\n' && c != EOF); // Limpa o buffer de entrada
+  while ((c = getchar()) != '\n' && c != EOF)
+    ; // Limpa o buffer de entrada
 }
--- banco.h
+++ banco.h
@@ -1,23 +1,31 @@
 #define TOTAL 1000
 #define NOME 50
-#define CONTA 5
+#define CONTA 10
 #define SENHA 10
 #define CPF 13
 
 typedef struct Cliente {
-    char nome[NOME];
-    char cpf[CPF];
-    char senha[SENHA];
-    int saldo;
-    char conta[CONTA];
+  char nome[NOME];
+  char cpf[CPF];
+  char senha[SENHA];
+  int saldo;
+  char conta[CONTA];
 } Cliente;
 
-typedef enum {OK, SENHA_CURTA, LIMITE_CLIENTES, CPF_INVALIDO, CLIENTE_NAO_ENCONTRADO} ERROS;
+typedef enum {
+  OK,
+  SENHA_CURTA,
+  LIMITE_CLIENTES,
+  CPF_INVALIDO,
+  CLIENTE_NAO_ENCONTRADO,
+  SEM_CLIENTES,
+  LISTA_VAZIA,
+} ERROS;
 
-typedef ERROS (*funcao)(Cliente[], int*);
+typedef ERROS (*funcao)(Cliente[], int *);
 
-ERROS criar (Cliente clientes[], int *pos);
+ERROS criar(Cliente clientes[], int *pos);
 ERROS deletar(Cliente clientes[], int *pos);
+ERROS listar(Cliente clientes[], int *pos);
 
 void clearBuffer();
-
